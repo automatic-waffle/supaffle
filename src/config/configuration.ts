4559785c5fd97c1abe51cf2d234108a99ca549fc
Reidond <reidond@gmail.com>
2021-07-13 01:02:04 +0300

    feat: cors

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,6 @@
 export default () => ({
   port: Number(process.env.PORT) || 3000,
+  corsWhitelist: process.env.CORS_WHITELIST.split(','),
   supabase: {
     apiKey: process.env.SUPABASE_API_KEY || '',
     apiUrl: process.env.SUPABASE_API_URL || '',
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module.js';
@@ -8,11 +9,26 @@ import { SupabaseService } from './supabase/supabase.service.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const whitelist: string[] = app.get(ConfigService).get('corsWhitelist');
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
+
   const supabase = app.get(SupabaseService).client;
   const arsenalPresetsService = app.get(ArsenalPresetsService);
 
   await app
-    .listen(3000)
+    .listen(4000)
     .then(() => loguxServer({ supabase, arsenalPresetsService }));
 }
 bootstrap();
